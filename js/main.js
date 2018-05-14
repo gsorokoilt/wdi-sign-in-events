1b5077ed24e9cf949a69ce8029bb58ca3fd1681e
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-05-14 19:02:13 -0400

    continue js file update

--- js/main.js
+++ js/main.js
@@ -25,7 +25,29 @@ var close = document.querySelector(".close");
 
   })
 
+  var user = document.querySelector("#user")
+  var password = document.querySelector("#pass")
 
 // defining click action for submit button
+var submit = document.querySelector(".submit")
+  submit.addEventListener('click', function(event){
+    var user = document.querySelector("#user")
+    var password = document.querySelector("#pass")
+    user.classList.add("error");
+    password.classList.add("error")
+  })
+
+  user.addEventListener('click', function(event){
+    if (user.classList.contains("error")){
+      user.classList.remove("error")}
+
+  })
+
+  password.addEventListener('click', function(event){
+    if (password.classList.contains("error")){
+      password.classList.remove("error")}
+
+  })
+
 
 });
